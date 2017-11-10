873af710f86625bfaffa259e0b20cb029e58c581
Sean Johnson <sean.johnson@digikey.com>
2017-11-10 10:53:20 -0600

    Redirect after POST, show all items in template and fix typo in functional test.

--- functional_tests.py
+++ functional_tests.py
@@ -47,7 +47,7 @@ class NewVisitorTest(unittest.TestCase):
         # enters "Use peacock featheres to make a fly" (Edith is very
         # methodical
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock featheres to make a fly')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
